a42090a425d7f67fbd1513b7cfa0bf622b71d3f8
jinj00oo <icems0428@naver.com>
2025-02-12 23:27:14 +0900

    Edit 페이지 추가

---
+++ day18/src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Notfound from "./pages/Notfound";
+
+function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+        <Link to={"/edit"}>Edit</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
